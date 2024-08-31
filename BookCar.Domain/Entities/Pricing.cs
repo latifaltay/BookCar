ff42dcc65a02208292b7c6e05a9da9f774563a59
Latif Altay <latifaltay00@gmail.com>
2024-08-31 17:10:52 +0300

    Domain Layer and Entities are done

---
+++ BookCar.Domain/Entities/Pricing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Domain.Entities
+{
+    public class Pricing
+    {
+        public int PricingId { get; set; }
+        public int Name { get; set; }
+        public List<CarPricing> CarPricing { get; set; }
+    }
+}
