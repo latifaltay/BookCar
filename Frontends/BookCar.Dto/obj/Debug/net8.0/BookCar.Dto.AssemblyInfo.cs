14613d4b11402fd9caada40c57a65818e4445cc3
Latif Altay <latifaltay00@gmail.com>
2024-09-11 20:54:22 +0300

    Service api consume is done

--- Frontends/BookCar.Dto/obj/Debug/net8.0/BookCar.Dto.AssemblyInfo.cs
+++ Frontends/BookCar.Dto/obj/Debug/net8.0/BookCar.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookCar.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53638051300c8bb0852434c105618a5dada238b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d223779865abe00564450cf5cabfc205db0ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookCar.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookCar.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Frontends/BookCar.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using BookCar.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookCar.WebUI.Controllers
+{
+    public class ServiceController(IHttpClientFactory _httpClientFactory) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7028/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
