7c22d74667ec1185bad65c07cc00502bcd9b4c8f
Latif Altay <latifaltay00@gmail.com>
2024-09-24 08:47:51 +0300

    New car create with admin panel operation is writed (113)

--- Frontends/BookCar.WebUI/Controllers/AdminCarController.cs
+++ Frontends/BookCar.WebUI/Controllers/AdminCarController.cs
@@ -3,6 +3,7 @@ using BookCar.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace BookCar.WebUI.Controllers
 {
@@ -37,6 +38,21 @@ namespace BookCar.WebUI.Controllers
                                                 }).ToList();
             ViewBag.BrandValues = brandValues;
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(CreateCarDto createCarDto) 
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCarDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7028/api/Cars/", stringContent);
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("Index");   
+            }
+            return View();
+
         }
 
 
--- Presentation/BookCar.WebApi/obj/Debug/net8.0/BookCar.WebApi.AssemblyInfo.cs
+++ Presentation/BookCar.WebApi/obj/Debug/net8.0/BookCar.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookCar.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+768971bf9add10bc7a2e06aa1cf8410d2665e63f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a69e6b5de9f1c9df3199af3f7f82a4ee2000122")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookCar.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookCar.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
