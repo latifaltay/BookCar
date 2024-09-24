27ba0c776bdc74136b5737b4b5bf7e33765d966c
Latif Altay <latifaltay00@gmail.com>
2024-09-24 04:52:16 +0300

    admin panel layout is created (105)

--- Presentation/BookCar.WebApi/obj/Debug/net8.0/BookCar.WebApi.AssemblyInfo.cs
+++ Presentation/BookCar.WebApi/obj/Debug/net8.0/BookCar.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookCar.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97976d7bb6cb23f42f113d6825aabe82421d954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a550ff056908c1ecf528bf44845cd42efe4a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookCar.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookCar.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
