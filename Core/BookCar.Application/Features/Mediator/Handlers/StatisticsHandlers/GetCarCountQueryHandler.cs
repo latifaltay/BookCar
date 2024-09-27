71c9b1faabcfc99fbd3cc2f21b4d09e755387d5c
Latif Altay <latifaltay00@gmail.com>
2024-09-27 09:31:47 +0300

    statistic mediator infrastructure created

---
+++ Core/BookCar.Application/Features/Mediator/Handlers/StatisticsHandlers/GetCarCountQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using BookCar.Application.Features.Mediator.Queries.StatisticsQueries;
+using BookCar.Application.Features.Mediator.Results.StatisticsResult;
+using BookCar.Application.Interfaces;
+using BookCar.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetCarCountQueryHandler(IRepository<Car> _repository) : IRequestHandler<GetCarCountQuery, GetCarCountQueryResult>
+    {
+        public Task<GetCarCountQueryResult> Handle(GetCarCountQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
